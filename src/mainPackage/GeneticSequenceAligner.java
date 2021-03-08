3a2c344bdeb87aa1ba5869a86ba0ff24191cd1e6
Chase Dawson <chase_dawson7@mymail.eku.edu>
2021-03-08 12:28:35 -0500

    - Added test genes - Added functional loops for when n < m ( rectangular matrix )

--- src/mainPackage/GeneticSequenceAligner.java
+++ src/mainPackage/GeneticSequenceAligner.java
@@ -3,18 +3,55 @@ package mainPackage;
 public class GeneticSequenceAligner {
 
 	public static void main(String[] args) {
-		char[] testGene1x = {'C','A','G','C','A','C','T','T','G','G','A','T','T','C','T','C','G','G'};
-		char[] testGene1y = {'C','A','G','C','G','T','G','G'};
-		
-		char[] testGene2x = {'T','C','C','C','A','G','T','T','A','T','G','T','C','A','G','G','G',
-							'G','A','C','A','C','G','A','G','C','A','T','G','C','A','G','A','G','A','C'};
-		char[] testGene2Y = {'A','A','T','T','G','C','C','G','C','C','G','T','C','G','T','T','T',
-							'T','C','A','G','C','A','G','T','T','A','T','G','T','C','A','G','A','T','C'};
+		char[] testGene1x = { 'C', 'A', 'G', 'C', 'A', 'C', 'T', 'T', 'G', 'G', 'A', 'T', 'T', 'C', 'T', 'C', 'G',
+				'G' };
+		char[] testGene1y = { 'C', 'A', 'G', 'C', 'G', 'T', 'G', 'G' };
+
+		char[] testGene2x = { 'T', 'C', 'C', 'C', 'A', 'G', 'T', 'T', 'A', 'T', 'G', 'T', 'C', 'A', 'G', 'G', 'G', 'G',
+				'A', 'C', 'A', 'C', 'G', 'A', 'G', 'C', 'A', 'T', 'G', 'C', 'A', 'G', 'A', 'G', 'A', 'C' };
+		char[] testGene2Y = { 'A', 'A', 'T', 'T', 'G', 'C', 'C', 'G', 'C', 'C', 'G', 'T', 'C', 'G', 'T', 'T', 'T', 'T',
+				'C', 'A', 'G', 'C', 'A', 'G', 'T', 'T', 'A', 'T', 'G', 'T', 'C', 'A', 'G', 'A', 'T', 'C' };
 
 	}
-	
-	private static char[][] getOptimalSequences(char[] geneX, int n, char[] geneY, int m){
-		int[][] optimalMatrix = new int[n+1][m+1];
+
+	private static char[][] getOptimalSequences(char[] geneX, int n, char[] geneY, int m) {
+		int[][] optimalMatrix = new int[n + 1][m + 1];
+
+		if (n > m) {
+		} else if (m > n) { // Matrix is a rectangle
+			
+			int diagonal = 1; // Length of the current diagonal
+			int vOffset = 0; // Vertical offset to determine starting point for diagonal iteration
+			int hOffset = 0; // Horizontal offset to determine starting point for diagonal iteration
+			int d = 1;
+			
+			// Iterates through the bottom right corner of the rectangular matrix
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < diagonal; j++) {
+					optimalMatrix[n - vOffset + j][m - j] = 0;// Insert the juice here;
+				}
+				vOffset++;
+				diagonal++;
+			}
+
+			// Iterates through the middle section of the rectangular matrix
+			for (int i = 0; i < m + 1 - n; i++) {
+				for (int j = 0; j < diagonal; j++) {
+					optimalMatrix[n - vOffset + j][m - hOffset - j] = 0;// Insert the juice here;
+				}
+				hOffset++;
+			}
+
+			// Iterates through the upper left corner of the rectangular matrix
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < diagonal; j++) {
+					optimalMatrix[n - vOffset + j][m - j] = 0;// Insert the juice here;
+				}
+				hOffset++;
+				diagonal--;
+			}
+		}
+
 	}
 
 }
